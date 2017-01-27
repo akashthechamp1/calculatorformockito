32172e78b407b54dd640b0fd1dd399ee366caa23
Akashmane <akash.mane@morningstar.com>
2017-01-27 12:06:20 +0530

    please do maven build

---
+++ src/tesst.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class tesst {
+	public static void main(String args[])
+	{double a ,b,c;
+		Scanner sc=new Scanner(System.in);
+	a=sc.nextDouble();
+	b=sc.nextDouble();
+	System.out.println(c=a+b);
+	}
+	
+}
